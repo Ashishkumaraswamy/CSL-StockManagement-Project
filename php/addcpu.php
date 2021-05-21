5ec58d1af602be7f8a7c8b78f18c378d8a814536
Ashish <ashishkumaraswamy@gmail.com>
2021-05-21 20:45:18 +0530

    changes made in adding part and system complaint

--- php/addcpu.php
+++ php/addcpu.php
@@ -28,7 +28,7 @@
 				while($i<=$cpuquant)
 				{
 					$cpuid=$cpucat.(string)($count+$i);
-					$insertcomp=mysqli_query($conn,"INSERT INTO cpu VALUES('{$cpuid}',{$purchaseid['purchaseid']},'{$ram}','{$procseries}',{$storage},1,1,'{$desc}')");
+					$insertcomp=mysqli_query($conn,"INSERT INTO cpu VALUES('{$cpuid}',{$purchaseid['purchaseid']},'{$ram}','{$procseries}',{$storage},1,1,'{$desc}','NA')");
 					$i=$i+1;
 				}
 				echo $cat." added to store. ID of the new items are from ".($count+1)." to ".($count+$i-1);
