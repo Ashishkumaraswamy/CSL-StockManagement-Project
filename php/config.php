050824f1e4bd6069b1a69591b4db60471c91c88c
Ashish <ashishkumaraswamy@gmail.com>
2021-07-05 09:54:38 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
-	$hostname= "";
-	$username="";
-	$password="";
-	$dbname="";
+	$hostname= "remotemysql.com";
+	$username="bJZvSixLNp";
+	$password="stMLQQ7kvK";
+	$dbname="bJZvSixLNp";
 
 	$conn=mysqli_connect($hostname, $username, $password, $dbname);
 	if(!$conn)
