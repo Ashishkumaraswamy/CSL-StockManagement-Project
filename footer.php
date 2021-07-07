a96a96625027f2887865cadc9a3d83bb8d0b0789
mathanamathav <62739618+mathanamathav@users.noreply.github.com>
2021-07-07 10:43:29 +0530

    small change

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="container-fluid padding" style="positon:fixed;bottom:0px;">
         <div class="row margin1">
             <div class="col-md-6 color_blue padding_footer">
-                <h4>Developed by *</h4>
+                <h4>Developed by DS 2k19</h4>
             </div>
             <div class="col-md-6 color_blue text-right padding_footer">
                 <h4>Copyright <i class="fa fa-copyright" aria-hidden="true"></i> CSL. All rights reserved</h4>
